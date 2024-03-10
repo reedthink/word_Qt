1ebea80a32f85ed2f94b403daa0d42225f736fc4
jkjk <reedthink@qq.com>
2024-03-10 20:57:33 +0800

    设置窗口布局

--- myword.cpp
+++ myword.cpp
@@ -1,8 +1,18 @@
 #include "myword.h"
+#include <QtWidgets>
 
 MyWord::MyWord(QWidget *parent)
     : QMainWindow(parent)
 {
+
+    mdiArea = new QMdiArea;
+    mdiArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    mdiArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    setCentralWidget(mdiArea);
+    move(200, 150);
+    resize(800, 500);
+
+    setWindowTitle(tr("Diy Word"));
 }
 
 MyWord::~MyWord()
